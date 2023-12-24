a5d24a8d946998db1a56e5a36adc9981c3dabe6d
Felipe Casal <felipecasalcornejo@gmail.com>
2023-12-24 18:50:09 -0300

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import "./NavBar.css"
+
+const NavBar = () =>{
+    return(
+        <>
+        <section className="container-navbar">
+
+        <h3>DirtBnB</h3>
+        <div>
+            <ul className="lista-nav">
+                <li className="opciones-nav">Home</li>
+                <li className="opciones-nav">hola</li>
+                <li className="opciones-nav"c>Contacto</li>
+            </ul>
+        </div>
+
+         
+        </section>
+         </>
+    )
+}
+
+export default NavBar;
